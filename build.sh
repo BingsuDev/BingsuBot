0312aa2627ef329b087df284bbf4ce07c663423b
LiveEmily <d.claassen2003@outlook.com>
2022-04-05 12:32:41 +0200

    Update build.sh and make it executable.

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ mkdir build && cd build
 touch .env
 cmake ..
 echo "TOKEN='<bot-token>'" > .env
-make
+make -j$(nproc --all)
 
-printf "You are now in the /build directory, please edit .env to add your bot's token, after that you can run ./BingsuBot and everything should work just fine!"
+printf "All set up! Please go into /build and edit .env to add your bot's token, after that you can run ./BingsuBot and everything should work just fine!"
