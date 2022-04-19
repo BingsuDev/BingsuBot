297d9ed1cfeb19172f7b353ad376aa914a4099c2
LiveEmily <d.claassen2003@outlook.com>
2022-04-19 14:10:17 +0200

    General improvements

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 #include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cstdio>
+#include <ctime>
 #include "sleepy_discord/sleepy_discord.h"
 #include "dotenv.h"
 
@@ -19,6 +24,7 @@ namespace Command {
     struct Command {
         std::string name;
         std::vector<std::string> params;
+        std::string description;
         Verb verb;
     };
     using MappedCommands = std::unordered_map<std::string, Command>;
@@ -34,83 +40,165 @@ class BingsuBot : public SleepyDiscord::DiscordClient {
         using SleepyDiscord::DiscordClient::DiscordClient;
         void onReady(SleepyDiscord::Ready ready) override {
             updateStatus("Getting developed!", 0, SleepyDiscord::Status::online, false);
+            updatePresence();
+        }
+
+        void updatePresence() {
+            while(1) {
+                std::srand(std::time(nullptr));
+                std::vector<std::string> statuses = {"Getting developed!", "Learning new commands!", "Helping out Emi!", "Contemplating life...", "Low level code ftw!"};
+                int i = rand() % (statuses.size() - 1 + 1);
+                int delay = 900;
+                delay *= CLOCKS_PER_SEC;
+                clock_t now = clock();
+                while(clock() - now <delay);
+                updateStatus(statuses[i], 0, SleepyDiscord::Status::online, false);
+            }
         }
 
         void onMember(SleepyDiscord::Snowflake<SleepyDiscord::Server> serverID, SleepyDiscord::ServerMember member) override {
-            SleepyDiscord::Channel welcomeChannel = getChannel("803931209054027787");
-            sendMessage(welcomeChannel, "Welcome " + member.user.username + "!", SleepyDiscord::Async);
+            SleepyDiscord::Embed embed;
+            SleepyDiscord::SendMessageParams messageParams;
+            std::vector<SleepyDiscord::EmbedField> fields;
+            SleepyDiscord::EmbedField field [1];
+            SleepyDiscord::EmbedImage image;
+            SleepyDiscord::EmbedThumbnail thumbnail;
+            image.url = "https://giffiles.alphacoders.com/127/127869.gif";
+            thumbnail.url = "https://cdn.discordapp.com/avatars/" + member.user.ID + "/" + member.user.avatar + ".png";
+            field[0].name = "Welcome!";
+            field[0].value = "Welcome to Emi's hideout kind stranger, we hope you will enjoy your stay and make lots of friends!";
+            fields.push_back(field[0]);
+            embed.title = member.user.username;
+            embed.image = image;
+            embed.thumbnail = thumbnail;
+            embed.fields = fields;
+
+            messageParams.channelID = "965878907339436032";
+            messageParams.embed = embed;
+
+            try {
+                sendMessage(messageParams, SleepyDiscord::Async);
+            } catch(SleepyDiscord::ErrorCode e) {
+                std::cout << e << std::endl;
+            }
+            fields.clear();
         }
 
         void onMessage(SleepyDiscord::Message message) override {
-            if(message.isMentioned(getID())) {
-                std::queue<std::string> parameters = split(message.content);
-                const std::string mention = "<@" + getID().string() + ">";
-                const std::string mentionNick = "<@!" + getID().string() + ">";
+            auto& dotenv = dotenv::env.load_dotenv();
+            std::queue<std::string> parameters = split(message.content);
+            const std::string mention = "<@" + getID().string() + ">";
+            const std::string mentionNick = "<@!" + getID().string() + ">";
+            const char prefix = dotenv["PREFIX"].front();
 
-                if(parameters.size() <= 1 && (parameters.front() != mention || parameters.front() != mentionNick)) return;
+            if(message.author.bot == true) return;
 
-                parameters.pop();
-                if(parameters.empty()) return;
+            if(parameters.empty()) return;
 
-                Command::MappedCommands::iterator foundCommand = Command::all.find(parameters.front());
-                if(foundCommand == Command::all.end()) {
-                    sendMessage(message.channelID, "Error: Command not found!", SleepyDiscord::Async);
-                    return;
-                }
-                parameters.pop();
-                if(parameters.size() < foundCommand->second.params.size()) {
-                    sendMessage(message.channelID, "Error: Too few parameters!", SleepyDiscord::Async);
-                    return;
-                }
+            if(parameters.front()[0] != prefix) return;
 
-                foundCommand->second.verb(*this, message, parameters);
+            parameters.front().erase(0,1);
+
+            if(parameters.empty()) return;
+
+            Command::MappedCommands::iterator foundCommand = Command::all.find(parameters.front());
+            if(foundCommand == Command::all.end()) {
+                sendMessage(message.channelID, "Error: Command not found!", SleepyDiscord::Async);
+                return;
             }
+            parameters.pop();
+            if(parameters.size() < foundCommand->second.params.size()) {
+                sendMessage(message.channelID, "Error: Too few parameters!", SleepyDiscord::Async);
+                return;
+            }
+
+            foundCommand->second.verb(*this, message, parameters);
         }
 };
 
 int main(int argc, char** argv) {
     Command::addCommand({
-        "help", {}, [](
+        "help", {}, "This command right here.", [](
             BingsuBot& client,
             SleepyDiscord::Message& message,
             std::queue<std::string>&
         ) {
-            constexpr char start[] = "Here's a list of all command:```\n";
-            constexpr char theEnd[] = "```";
-            std::size_t length = strlen(start) + strlen(theEnd);
+            SleepyDiscord::Embed embed;
+            std::vector<SleepyDiscord::EmbedField> fields;
+            SleepyDiscord::SendMessageParams messageParams;
+            SleepyDiscord::EmbedField field [10];
+            embed.title = "Help!";
+            int i = 0;
             for(Command::MappedCommand& command : Command::all) {
-                length += command.first.size();
-                length += 2;
-                for(std::string& paramName : command.second.params) {
-                    length += 2;
-                    length += paramName.size();
-                }
+                field[i].name = command.second.name;
+                field[i].value = command.second.description;
+                fields.push_back(field[i]);
+                i++;
             }
+            embed.fields = fields;
 
-            std::string output;
-            output.reserve(length);
-            output += start;
-            for(Command::MappedCommand& command : Command::all) {
-                output += command.first;
-                output += ' ';
-                for(std::string& paramName : command.second.params) {
-                    output += '<';
-                    output += paramName;
-                    output += "> ";
-                }
-                output += '\n';
+            messageParams.channelID = message.channelID;
+            messageParams.embed = embed;
+
+            try {
+                client.sendMessage(messageParams, SleepyDiscord::Async);
+            } catch(SleepyDiscord::ErrorCode e) {
+                std::cout << e << std::endl;
             }
-            output += theEnd;
-            client.sendMessage(message.channelID, output, SleepyDiscord::Async);
+            fields.clear();
         }
     });
     Command::addCommand({
-        "stop", {}, [](
+        "avatar", {}, "Request someone's avatar", [](
+            BingsuBot& client,
+            SleepyDiscord::Message& message,
+            std::queue<std::string>&
+        ) {
+            SleepyDiscord::Embed embed;
+            std::string name;
+            SleepyDiscord::EmbedImage image;
+            SleepyDiscord::SendMessageParams messageParams;
+            messageParams.channelID = message.channelID;
+            name = message.author.username;
+            image.url = "https://cdn.discordapp.com/avatars/" + message.author.ID + "/" + message.author.avatar + ".png";
+            if(message.mentions.size() >= 1) {
+                name = message.mentions[0].username;
+                image.url = "https://cdn.discordapp.com/avatars/" + message.mentions[0].ID + "/" + message.mentions[0].avatar + ".png";
+            }
+
+            embed.image = image;
+            embed.title = name;
+
+            try {
+                messageParams.embed = embed;
+                client.sendMessage(messageParams, SleepyDiscord::Async);
+            } catch(SleepyDiscord::ErrorCode e) {
+                std::cout << e << std::endl;
+            }
+        }
+    });
+    /*Command::addCommand({
+        "ping", {}, "Test the bots latency!", [](
             BingsuBot& client,
             SleepyDiscord::Message& message,
             std::queue<std::string>& params
         ) {
-            exit(0);
+            std::time_t t = std::time(0);
+            std::tm* now = std::localtime(&t);
+            std::cout << (now->tm_min) << ":" << (now->tm_sec) << std::endl;
+            std::cout << message.timestamp.substr(14).substr(0, message.timestamp.substr(14).find(".")) << std::endl;
+            client.sendMessage(message.channelID, (now->tm_min) + ":" + (now->tm_sec), SleepyDiscord::Async);
+            client.sendMessage(message.channelID, message.timestamp.substr(14).substr(0, message.timestamp.substr(14).find(".")), SleepyDiscord::Async);
+        }
+    });*/
+    Command::addCommand({
+        "stop", {}, "Stops the bot", [](
+            BingsuBot& client,
+            SleepyDiscord::Message& message,
+            std::queue<std::string>& params
+        ) {
+            client.sendMessage(message.channelID, "Stopping the bot...");
+            abort();
         }
     });
 
